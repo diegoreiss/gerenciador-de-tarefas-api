4471f2714041073f8c45b1bec705a1b0264059b2
diegoreiss <diegoportal.reis@outlook.com>
2023-06-11 23:01:50 -0300

    add Repository usuario_repository

---
+++ Src/Infra/Repository/usuario_repository.py
@@ -0,0 +1,63 @@
+from Src.Infra.Configs.connection import DBConnectionHandler
+from Src.Infra.Entities.usuario import Usuario
+
+
+class UsuarioRepository:
+    @staticmethod
+    def select_all():
+        with DBConnectionHandler() as db:
+            return db.session.query(Usuario).all()
+
+    @staticmethod
+    def select(id: int):
+        with DBConnectionHandler() as db:
+            return db.session.query(Usuario).filter(Usuario.id == id).first()
+
+    @staticmethod
+    def insert(**kwargs) -> Usuario:
+        with DBConnectionHandler() as db:
+            print('entrei')
+            data_insert = Usuario(**kwargs)
+            db.session.add(data_insert)
+            db.session.commit()
+            db.session.refresh(data_insert)
+
+            return data_insert
+
+    @staticmethod
+    def delete(id: int):
+        with DBConnectionHandler() as db:
+            usuario = db.session.query(Usuario).filter(Usuario.id == id)
+
+            if usuario.first() is None:
+                return 404
+
+            usuario.delete(synchronize_session=False)
+            db.session.commit()
+
+    @staticmethod
+    def update(id: int, usuario: Usuario) -> int | Usuario:
+
+        with DBConnectionHandler() as db:
+            usuario_query = db.session.query(Usuario).filter(Usuario.id == id)
+
+            if usuario_query.first() is None:
+                return 404
+
+            try:
+                usuario_query.update(usuario.dict(), synchronize_session=False)
+                db.session.commit()
+            except BaseException as e:
+                raise e
+
+            return usuario_query.first()
+
+    @staticmethod
+    def get_usuario_by_nome_login(nome_login: str) -> int | Usuario:
+        with DBConnectionHandler() as db:
+            usuario_query = db.session.query(Usuario).filter(Usuario.nome_login == nome_login).first()
+
+            if not usuario_query:
+                return 404
+
+            return usuario_query
