67fc1f82d6eb6cb7953e49af167e361524fd5fb0
diegoreiss <diegoportal.reis@outlook.com>
2023-06-15 17:48:07 -0300

    mudando tipagens e constraints

--- db_scripts/script.sql
+++ db_scripts/script.sql
@@ -2,18 +2,18 @@ SET NAMES utf8;
 
 DROP TABLE IF EXISTS funcao;
 CREATE TABLE IF NOT EXISTS funcao(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome VARCHAR(30) NOT NULL
 );
 
 DROP TABLE IF EXISTS usuario;
 CREATE TABLE IF NOT EXISTS usuario(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome VARCHAR(40) NOT NULL,
     sobrenome VARCHAR(40) NOT NULL,
     nome_login VARCHAR(60) NOT NULL,
     senha_hash TEXT NOT NULL,
-    funcao_id INT NOT NULL,
+    funcao_id INTEGER NOT NULL,
 
     CONSTRAINT fk_usuario_funcao
 	   FOREIGN KEY (funcao_id)
@@ -22,28 +22,30 @@ CREATE TABLE IF NOT EXISTS usuario(
 
 DROP TABLE IF EXISTS turma;
 CREATE TABLE IF NOT EXISTS turma(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome VARCHAR(60) NOT NULL
 );
 
 DROP TABLE IF EXISTS professor_turma;
 CREATE TABLE IF NOT EXISTS professor_turma(
-    usuario_professor_id INT NOT NULL,
-    turma_id INT NOT NULL,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    usuario_professor_id INTEGER NOT NULL,
+    turma_id INTEGER NOT NULL,
 
     CONSTRAINT fk_usuario_professor_turma
         FOREIGN KEY (usuario_professor_id)
         REFERENCES usuario(id),
     
-    CONSTRAINT fk_turma_turma_professor
+    CONSTRAINT fk_turma_professor_turma
         FOREIGN KEY (turma_id)
         REFERENCES turma(id)
 );
 
 DROP TABLE IF EXISTS aluno_turma;
 CREATE TABLE IF NOT EXISTS aluno_turma(
-    usuario_aluno_id INT NOT NULL,
-    turma_id INT NOT NULL,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    usuario_aluno_id INTEGER NOT NULL,
+    turma_id INTEGER NOT NULL,
 
     CONSTRAINT fk_usuario_aluno_turma
         FOREIGN KEY (usuario_aluno_id)
@@ -56,30 +58,30 @@ CREATE TABLE IF NOT EXISTS aluno_turma(
 
 DROP TABLE IF EXISTS tarefa;
 CREATE TABLE IF NOT EXISTS tarefa(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     titulo VARCHAR(60) NOT NULL,
     descricao TEXT NULL,
-    status ENUM("Ativo", "Inativo") DEFAULT ("Ativo"),
-    id_turma INT NOT NULL,
+    status ENUM("ativo", "inativo") DEFAULT ("ativo"),
+    turma_id INTEGER NOT NULL,
 
     CONSTRAINT fk_tarefa_turma
-        FOREIGN KEY (id_turma)
+        FOREIGN KEY (turma_id)
         REFERENCES turma(id)
 );
 
 DROP TABLE IF EXISTS comentario;
 CREATE TABLE IF NOT EXISTS comentario(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     texto TEXT NOT NULL,
-    id_usuario INT NOT NULL,
-    id_tarefa INT NOT NULL,
+    usuario_id INTEGER NOT NULL,
+    tarefa_id INTEGER NOT NULL,
 
     CONSTRAINT fk_comentario_usuario
-        FOREIGN KEY (id_usuario)
+        FOREIGN KEY (usuario_id)
         REFERENCES usuario(id),
 
     CONSTRAINT fk_comentario_tarefa
-        FOREIGN KEY (id_tarefa)
+        FOREIGN KEY (tarefa_id)
         REFERENCES tarefa(id)
 );
 
