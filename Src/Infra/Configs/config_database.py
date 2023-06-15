7acb8205daabd898e3cab621edfd62644b2ef27f
diegoreiss <diegoportal.reis@outlook.com>
2023-06-15 17:54:47 -0300

    mudando estrutura configDatabase

--- Src/Infra/Configs/config_database.py
+++ Src/Infra/Configs/config_database.py
@@ -2,23 +2,24 @@ import os
 from configparser import ConfigParser
 
 
-def config_database(section: str = 'mysql') -> str:
-    def get_string_connection(db: dict) -> str:
+class ConfigDatabase(ConfigParser):
+    def get_string_connection(self, section: str = 'mysql') -> str:
+        full_file_path = os.path.join(os.getcwd(), 'database.ini')
+
+        self.read(full_file_path)
+
+        if self.has_section(section):
+            params = self.items(section)
+
+            db_params = {param[0]: param[1] for param in params}
+
+            return ConfigDatabase.__format_db_string_connection(db_params)
+
+    @staticmethod
+    def __format_db_string_connection(params: dict) -> str:
         pattern = 'dialect+driver://username:password@host:port/database'
 
-        for key, value in db.items():
+        for key, value in params.items():
             pattern = pattern.replace(key, value)
 
         return pattern
-
-    full_file_path = os.path.join(os.getcwd(), 'database.ini')
-
-    parser = ConfigParser()
-    parser.read(full_file_path)
-
-    if parser.has_section(section):
-        params = parser.items(section)
-
-        db_params = {param[0]: param[1] for param in params}
-
-        return get_string_connection(db_params)
