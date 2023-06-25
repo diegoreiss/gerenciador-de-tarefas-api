cb345b916d62ae148ab1eae90f19288fc955ba22
diegoreiss <diegoportal.reis@outlook.com>
2023-06-25 11:18:47 -0300

    update funcao

--- Src/Infra/Entities/funcao.py
+++ Src/Infra/Entities/funcao.py
@@ -1,9 +1,12 @@
 from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 from Src.Infra.Configs.base import Base
 
 
 class Funcao(Base):
-    __tablename__ = 'funcao'
+    __tablename__ = "funcao"
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     nome = Column(String(30), nullable=False)
+
+    usuario_children = relationship("Usuario", back_populates="funcao_parent")
