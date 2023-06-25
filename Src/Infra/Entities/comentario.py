972ea105ed345066c4f5d989d6102f0e090180c2
diegoreiss <diegoportal.reis@outlook.com>
2023-06-25 11:19:37 -0300

    update comentario

--- Src/Infra/Entities/comentario.py
+++ Src/Infra/Entities/comentario.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Text, Integer, ForeignKeyConstraint
+from sqlalchemy.orm import relationship
 from Src.Infra.Configs.base import Base
 
 
@@ -6,17 +7,20 @@ class Comentario(Base):
     __tablename__ = 'comentario'
     __table_args__ = (
         ForeignKeyConstraint(
-            ("usuario_id",), ["usuario.id"],
-            name="fk_comentario_usuario"
+            ("tarefa_id",), ["tarefa.id"],
+            name="fk_comentario_tarefa", ondelete="CASCADE"
         ),
         ForeignKeyConstraint(
-            ("tarefa_id",), ["tarefa.id"],
-            name="kf_comentario_tarefa"
+            ("usuario_id",), ["usuario.id"],
+            name="fk_comentario_usuario", ondelete="CASCADE"
         )
     )
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     texto = Column(Text, nullable=False)
 
-    usuario_id = Column(Integer, nullable=False)
     tarefa_id = Column(Integer, nullable=False)
+    usuario_id = Column(Integer, nullable=False)
+
+    tarefa_parent = relationship("Tarefa", back_populates="comentario_children")
+    usuario_parent = relationship("Usuario", back_populates="comentario_children")
