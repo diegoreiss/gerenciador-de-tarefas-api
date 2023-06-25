f8bd06e0fbff686d81db4bfd88ad3ec69b742caf
diegoreiss <diegoportal.reis@outlook.com>
2023-06-25 11:23:13 -0300

    add tarefa schema

---
+++ Src/Api/Schemas/tarefa_schema.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from pydantic import BaseModel, Extra
+from Src.Infra.Entities.tarefa import Status, Prioridade
+
+
+class Tarefa(BaseModel):
+    titulo: str
+    descricao: str
+    status: Status
+    prioridade: Prioridade
+    anexo: Optional[str] = None
+
+
+class TarefaResponse(Tarefa):
+    id: int
+    usuario_autor_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class TarefaUpdate(BaseModel):
+    titulo: str
+    descricao: str
+    status: Status
+    prioridade: Prioridade
+
+    class Config:
+        extra = Extra.allow
