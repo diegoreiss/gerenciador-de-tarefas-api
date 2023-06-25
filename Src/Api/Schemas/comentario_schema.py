28e8ac26d930b0f1152e2d409dbc3a26d790c401
diegoreiss <diegoportal.reis@outlook.com>
2023-06-25 11:23:30 -0300

    add comentario schema

---
+++ Src/Api/Schemas/comentario_schema.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Extra
+
+
+class Comentario(BaseModel):
+    texto: str
+    tarefa_id: int
+
+    class Config:
+        extra = Extra.allow
+
+
+class ComentarioResponse(Comentario):
+    usuario_id: int
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class ComentarioViewResponse(BaseModel):
+    id: int
+    funcao: str
+    nome_login: str
+    texto: str
