367c9806545f9a6b6a27b5ae1817bf86cbbcb73a
diegoreiss <diegoportal.reis@outlook.com>
2023-06-26 13:27:37 -0300

    add defesa para falta de conexao com o banco

--- Src/Api/Routers/auth.py
+++ Src/Api/Routers/auth.py
@@ -8,20 +8,22 @@ router = APIRouter(tags=['Autenticação'])
 
 @router.post('/login')
 def login(user_credentials: OAuth2PasswordRequestForm = Depends()):
-    usuario = UsuarioRepository.get_usuario_by_nome_login(user_credentials.username)
+    try:
+        usuario = UsuarioRepository.get_usuario_by_nome_login(user_credentials.username)
 
-    if usuario == 404:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail=f'Credenciais Inválidas')
+        if usuario == 404:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                                detail=f'Credenciais Inválidas')
 
-    if not password_utils.verify_password(user_credentials.password, usuario.senha_hash):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail='Credenciais Inválidas')
+        if not password_utils.verify_password(user_credentials.password, usuario.senha_hash):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                                detail='Credenciais Inválidas')
 
-    # criar um token
-    dados_para_o_token = {"usuario_id": usuario.id}
-    token_de_acesso = oauth2.criar_acesso_token(data=dados_para_o_token)
+        dados_para_o_token = {"usuario_id": usuario.id}
+        token_de_acesso = oauth2.criar_acesso_token(data=dados_para_o_token)
 
-    # retornar o token
-
-    return {"access_token": token_de_acesso, "token_type": "Bearer"}
+        return {"access_token": token_de_acesso, "token_type": "Bearer"}
+    except BaseException as e:
+        if "Can't connect" in str(e):
+            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                                detail="Sem conexão com o banco de dados do sitema")
