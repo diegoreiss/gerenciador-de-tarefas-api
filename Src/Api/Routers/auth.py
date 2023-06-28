12e47b2ba81d1aeb8123f1bf374aea2d0b5839d6
diegoreiss <diegoportal.reis@outlook.com>
2023-06-28 04:18:39 -0300

    resolvendo bug HTTPException

--- Src/Api/Routers/auth.py
+++ Src/Api/Routers/auth.py
@@ -8,22 +8,19 @@ router = APIRouter(tags=['Autenticação'])
 
 @router.post('/login')
 def login(user_credentials: OAuth2PasswordRequestForm = Depends()):
+    usuario = None
     try:
         usuario = UsuarioRepository.get_usuario_by_nome_login(user_credentials.username)
-
-        if usuario == 404:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                                detail=f'Credenciais Inválidas')
-
-        if not password_utils.verify_password(user_credentials.password, usuario.senha_hash):
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                                detail='Credenciais Inválidas')
-
-        dados_para_o_token = {"usuario_id": usuario.id}
-        token_de_acesso = oauth2.criar_acesso_token(data=dados_para_o_token)
-
-        return {"access_token": token_de_acesso, "token_type": "Bearer"}
     except BaseException as e:
+        print(e)
         if "Can't connect" in str(e):
             raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
                                 detail="Sem conexão com o banco de dados do sitema")
+
+    if usuario in (404, None) or not password_utils.verify_password(user_credentials.password, usuario.senha_hash):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Credenciais Inválidas")
+
+    dados_para_o_token = {"usuario_id": usuario.id}
+    token_de_acesso = oauth2.criar_acesso_token(data=dados_para_o_token)
+
+    return {"access_token": token_de_acesso, "token_type": "Bearer"}
